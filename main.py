0a5b8017f0b62f477b145ea2e2112abd6f5d8077
Jan Hybs <jan.hybs@tul.cz>
2015-04-25 22:30:11 +0200

    Add caches in Jablonec

--- main.py
+++ main.py
@@ -51,15 +51,45 @@ class Geo(object):
 
 
 if __name__ == "__main__":
-    urls = [
-        "http://www.geocaching.com/geocache/GC4JW3M_cyklostezka-varhany-oldrichovska-vi",
-        "http://www.geocaching.com/geocache/GC4JW6G_cyklostezka-varhany-oldrichovska-xi",
-        "http://www.geocaching.com/geocache/GC4JW62_cyklostezka-varhany-oldrichovska-x",
-        "http://www.geocaching.com/geocache/GC4JW4Z_cyklostezka-varhany-oldrichovska-ix",
-        "http://www.geocaching.com/geocache/GC4JW4R_cyklostezka-varhany-oldrichovska-viii",
-        "http://www.geocaching.com/geocache/GC4JW4A_cyklostezka-varhany-oldrichovska-vii"
-            ]
-    message = ""
+    urls = ['http://www.geocaching.com/geocache/GC4XHWT_dum-cesko-nemeckeho-porozumeni', 'http://www.geocaching.com/geocache/GC2KM4G_spoutana-nisa', 'http://www.geocaching.com/geocache/GC26DB4_areal-volnocasovych-aktivit-celakovskeho', 'http://www.geocaching.com/geocache/GC573ZY_izs-v-lk-stanice-hzs-jablonec-nad-nisou', 'http://www.geocaching.com/geocache/GC5HZJZ_pan-vajicko', 'http://www.geocaching.com/geocache/GC2N9PN_oko-bere', 'http://www.geocaching.com/geocache/GC1E0WP_hori', 'http://www.geocaching.com/geocache/GC1MY27_javor-ve-msene', 'http://www.geocaching.com/geocache/GC54KK9_sidliste-s-barvami-duhy', 'http://www.geocaching.com/geocache/GC2W50Q_pepicek-jde-do-skolky', 'http://www.geocaching.com/geocache/GC5BRV7_bobik', 'http://www.geocaching.com/geocache/GC4PYM3_kostel-nejsvetejsi-trojice', 'http://www.geocaching.com/geocache/GC4P2RZ_budka-pro-roryse', 'http://www.geocaching.com/geocache/GC2JCZF_draci-pribeh-i-evropska-legenda', 'http://www.geocaching.com/geocache/GC2M9AP_modra-je-dobra', 'http://www.geocaching.com/geocache/GC4ET7A_mseno-nad-nisou', 'http://www.geocaching.com/geocache/GC5GG9K_0-5-l-vody', 'http://www.geocaching.com/geocache/GC1DVEF_magicky-strom-magic-tree', 'http://www.geocaching.com/geocache/GC4PMVW_nefnukej-veverko', 'http://www.geocaching.com/geocache/GC2E3DR_paternity-2', 'http://www.geocaching.com/geocache/GC19GYE_prehradni-zahada', 'http://www.geocaching.com/geocache/GC2QK1P_tajvan', 'http://www.geocaching.com/geocache/GC2KN3R_singltrek', 'http://www.geocaching.com/geocache/GC1C8BP_haskovy-vily', 'http://www.geocaching.com/geocache/GC4FVQY_filmova', 'http://www.geocaching.com/geocache/GC1MF58_the-sculptures-of-jablonec-nad-nisou', 'http://www.geocaching.com/geocache/GC47J74_aprilek', 'http://www.geocaching.com/geocache/GC1KY0N_ceska-mincovna-the-czech-mint', 'http://www.geocaching.com/geocache/GCZYRN_prehrada-mseno-mseno-dam', 'http://www.geocaching.com/geocache/GC5JJZG_slunecni-hodiny', 'http://www.geocaching.com/geocache/GC2P455_rychla-smrt', 'http://www.geocaching.com/geocache/GC4EBY7_schody-do-pekla', 'http://www.geocaching.com/geocache/GC1P7NT_jizdarna-hippodrome', 'http://www.geocaching.com/geocache/GC2XG8M_lesni-tramvaj-ii-waldbim-ii']
+    urls = ['http://www.geocaching.com/geocache/GC4XHWT_dum-cesko-nemeckeho-porozumeni']
+    """[
+    "http://www.geocaching.com/geocache/GC2XG8M_lesni-tramvaj-ii-waldbim-ii",
+    "http://www.geocaching.com/geocache/GC1P7NT_jizdarna-hippodrome",
+    "http://www.geocaching.com/geocache/GC4EBY7_schody-do-pekla",
+    "http://www.geocaching.com/geocache/GC2P455_rychla-smrt",
+    "http://www.geocaching.com/geocache/GC5JJZG_slunecni-hodiny",
+    "http://www.geocaching.com/geocache/GCZYRN_prehrada-mseno-mseno-dam",
+    "http://www.geocaching.com/geocache/GC1KY0N_ceska-mincovna-the-czech-mint",
+    "http://www.geocaching.com/geocache/GC47J74_aprilek",
+    "http://www.geocaching.com/geocache/GC1MF58_the-sculptures-of-jablonec-nad-nisou",
+    "http://www.geocaching.com/geocache/GC4FVQY_filmova",
+    "http://www.geocaching.com/geocache/GC1C8BP_haskovy-vily",
+    "http://www.geocaching.com/geocache/GC2KN3R_singltrek",
+    "http://www.geocaching.com/geocache/GC2QK1P_tajvan",
+    "http://www.geocaching.com/geocache/GC19GYE_prehradni-zahada",
+    "http://www.geocaching.com/geocache/GC2E3DR_paternity-2",
+    "http://www.geocaching.com/geocache/GC4PMVW_nefnukej-veverko",
+    "http://www.geocaching.com/geocache/GC1DVEF_magicky-strom-magic-tree",
+    "http://www.geocaching.com/geocache/GC5GG9K_0-5-l-vody",
+    "http://www.geocaching.com/geocache/GC4ET7A_mseno-nad-nisou",
+    "http://www.geocaching.com/geocache/GC2M9AP_modra-je-dobra",
+    "http://www.geocaching.com/geocache/GC2JCZF_draci-pribeh-i-evropska-legenda",
+    "http://www.geocaching.com/geocache/GC4P2RZ_budka-pro-roryse",
+    "http://www.geocaching.com/geocache/GC4PYM3_kostel-nejsvetejsi-trojice",
+    "http://www.geocaching.com/geocache/GC5BRV7_bobik",
+    "http://www.geocaching.com/geocache/GC2W50Q_pepicek-jde-do-skolky",
+    "http://www.geocaching.com/geocache/GC54KK9_sidliste-s-barvami-duhy",
+    "http://www.geocaching.com/geocache/GC1MY27_javor-ve-msene",
+    "http://www.geocaching.com/geocache/GC1E0WP_hori",
+    "http://www.geocaching.com/geocache/GC2N9PN_oko-bere",
+    "http://www.geocaching.com/geocache/GC5HZJZ_pan-vajicko",
+    "http://www.geocaching.com/geocache/GC573ZY_izs-v-lk-stanice-hzs-jablonec-nad-nisou",
+    "http://www.geocaching.com/geocache/GC26DB4_areal-volnocasovych-aktivit-celakovskeho",
+    "http://www.geocaching.com/geocache/GC2KM4G_spoutana-nisa",
+    "http://www.geocaching.com/geocache/GC4XHWT_dum-cesko-nemeckeho-porozumeni"
+    ]"""
+    message = "Po dlouhem cekani nastal cas planovaneho vyletu do Jablonce k prehrade. Spolu s cz_1shark1 a cz_Yetti jsme se chladneho sobotniho rana dopravili k prehrade a posbirali nejake ty krabicky. Peclive pripravenou trasu kolem prehrady, jsme jeste navic museli obohatit keskami 'pri ceste'. Na konci dne jsme slavili peknych 34 kesi. Diky za pozvani a dalsi bodik."
     g = Geo()
     print 'login'
     g.login('', '')
