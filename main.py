ad5f2a7449c083d19ac651a26b7a76bbec1e0026
x3mSpeedy <jan.hybs@tul.cz>
2015-09-04 11:44:55 +0200

    Update lst

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ import webbrowser
 from mechanize import *
 import sys
 from bs4 import BeautifulSoup, NavigableString
+import json
 
+from encodings import hex_codec
 
 class Geo(object):
     br = Browser()
@@ -39,9 +41,7 @@ class Geo(object):
 
         if tb:
             self.br[
-                "ctl00$ContentBody$LogBookPanel1$uxTrackables$hdnSelectedActions"] = "2045329_Visited,4996238_Visited,"
-            self.br["ctl00$ContentBody$LogBookPanel1$uxTrackables$repTravelBugs$ctl01$ddlAction"] = [
-                '2045329_Visited', ]
+                "ctl00$ContentBody$LogBookPanel1$uxTrackables$hdnSelectedActions"] = "4996238_Visited"
             self.br["ctl00$ContentBody$LogBookPanel1$uxTrackables$repTravelBugs$ctl02$ddlAction"] = [
                 '4996238_Visited', ]
 
@@ -52,48 +52,26 @@ class Geo(object):
 
 if __name__ == "__main__":
     urls = ['http://www.geocaching.com/geocache/GC4XHWT_dum-cesko-nemeckeho-porozumeni', 'http://www.geocaching.com/geocache/GC2KM4G_spoutana-nisa', 'http://www.geocaching.com/geocache/GC26DB4_areal-volnocasovych-aktivit-celakovskeho', 'http://www.geocaching.com/geocache/GC573ZY_izs-v-lk-stanice-hzs-jablonec-nad-nisou', 'http://www.geocaching.com/geocache/GC5HZJZ_pan-vajicko', 'http://www.geocaching.com/geocache/GC2N9PN_oko-bere', 'http://www.geocaching.com/geocache/GC1E0WP_hori', 'http://www.geocaching.com/geocache/GC1MY27_javor-ve-msene', 'http://www.geocaching.com/geocache/GC54KK9_sidliste-s-barvami-duhy', 'http://www.geocaching.com/geocache/GC2W50Q_pepicek-jde-do-skolky', 'http://www.geocaching.com/geocache/GC5BRV7_bobik', 'http://www.geocaching.com/geocache/GC4PYM3_kostel-nejsvetejsi-trojice', 'http://www.geocaching.com/geocache/GC4P2RZ_budka-pro-roryse', 'http://www.geocaching.com/geocache/GC2JCZF_draci-pribeh-i-evropska-legenda', 'http://www.geocaching.com/geocache/GC2M9AP_modra-je-dobra', 'http://www.geocaching.com/geocache/GC4ET7A_mseno-nad-nisou', 'http://www.geocaching.com/geocache/GC5GG9K_0-5-l-vody', 'http://www.geocaching.com/geocache/GC1DVEF_magicky-strom-magic-tree', 'http://www.geocaching.com/geocache/GC4PMVW_nefnukej-veverko', 'http://www.geocaching.com/geocache/GC2E3DR_paternity-2', 'http://www.geocaching.com/geocache/GC19GYE_prehradni-zahada', 'http://www.geocaching.com/geocache/GC2QK1P_tajvan', 'http://www.geocaching.com/geocache/GC2KN3R_singltrek', 'http://www.geocaching.com/geocache/GC1C8BP_haskovy-vily', 'http://www.geocaching.com/geocache/GC4FVQY_filmova', 'http://www.geocaching.com/geocache/GC1MF58_the-sculptures-of-jablonec-nad-nisou', 'http://www.geocaching.com/geocache/GC47J74_aprilek', 'http://www.geocaching.com/geocache/GC1KY0N_ceska-mincovna-the-czech-mint', 'http://www.geocaching.com/geocache/GCZYRN_prehrada-mseno-mseno-dam', 'http://www.geocaching.com/geocache/GC5JJZG_slunecni-hodiny', 'http://www.geocaching.com/geocache/GC2P455_rychla-smrt', 'http://www.geocaching.com/geocache/GC4EBY7_schody-do-pekla', 'http://www.geocaching.com/geocache/GC1P7NT_jizdarna-hippodrome', 'http://www.geocaching.com/geocache/GC2XG8M_lesni-tramvaj-ii-waldbim-ii']
-    urls = ['http://www.geocaching.com/geocache/GC4XHWT_dum-cesko-nemeckeho-porozumeni']
-    """[
-    "http://www.geocaching.com/geocache/GC2XG8M_lesni-tramvaj-ii-waldbim-ii",
-    "http://www.geocaching.com/geocache/GC1P7NT_jizdarna-hippodrome",
-    "http://www.geocaching.com/geocache/GC4EBY7_schody-do-pekla",
-    "http://www.geocaching.com/geocache/GC2P455_rychla-smrt",
-    "http://www.geocaching.com/geocache/GC5JJZG_slunecni-hodiny",
-    "http://www.geocaching.com/geocache/GCZYRN_prehrada-mseno-mseno-dam",
-    "http://www.geocaching.com/geocache/GC1KY0N_ceska-mincovna-the-czech-mint",
-    "http://www.geocaching.com/geocache/GC47J74_aprilek",
-    "http://www.geocaching.com/geocache/GC1MF58_the-sculptures-of-jablonec-nad-nisou",
-    "http://www.geocaching.com/geocache/GC4FVQY_filmova",
-    "http://www.geocaching.com/geocache/GC1C8BP_haskovy-vily",
-    "http://www.geocaching.com/geocache/GC2KN3R_singltrek",
-    "http://www.geocaching.com/geocache/GC2QK1P_tajvan",
-    "http://www.geocaching.com/geocache/GC19GYE_prehradni-zahada",
-    "http://www.geocaching.com/geocache/GC2E3DR_paternity-2",
-    "http://www.geocaching.com/geocache/GC4PMVW_nefnukej-veverko",
-    "http://www.geocaching.com/geocache/GC1DVEF_magicky-strom-magic-tree",
-    "http://www.geocaching.com/geocache/GC5GG9K_0-5-l-vody",
-    "http://www.geocaching.com/geocache/GC4ET7A_mseno-nad-nisou",
-    "http://www.geocaching.com/geocache/GC2M9AP_modra-je-dobra",
-    "http://www.geocaching.com/geocache/GC2JCZF_draci-pribeh-i-evropska-legenda",
-    "http://www.geocaching.com/geocache/GC4P2RZ_budka-pro-roryse",
-    "http://www.geocaching.com/geocache/GC4PYM3_kostel-nejsvetejsi-trojice",
-    "http://www.geocaching.com/geocache/GC5BRV7_bobik",
-    "http://www.geocaching.com/geocache/GC2W50Q_pepicek-jde-do-skolky",
-    "http://www.geocaching.com/geocache/GC54KK9_sidliste-s-barvami-duhy",
-    "http://www.geocaching.com/geocache/GC1MY27_javor-ve-msene",
-    "http://www.geocaching.com/geocache/GC1E0WP_hori",
-    "http://www.geocaching.com/geocache/GC2N9PN_oko-bere",
-    "http://www.geocaching.com/geocache/GC5HZJZ_pan-vajicko",
-    "http://www.geocaching.com/geocache/GC573ZY_izs-v-lk-stanice-hzs-jablonec-nad-nisou",
-    "http://www.geocaching.com/geocache/GC26DB4_areal-volnocasovych-aktivit-celakovskeho",
-    "http://www.geocaching.com/geocache/GC2KM4G_spoutana-nisa",
-    "http://www.geocaching.com/geocache/GC4XHWT_dum-cesko-nemeckeho-porozumeni"
-    ]"""
-    message = "Po dlouhem cekani nastal cas planovaneho vyletu do Jablonce k prehrade. Spolu s cz_1shark1 a cz_Yetti jsme se chladneho sobotniho rana dopravili k prehrade a posbirali nejake ty krabicky. Peclive pripravenou trasu kolem prehrady, jsme jeste navic museli obohatit keskami 'pri ceste'. Na konci dne jsme slavili peknych 34 kesi. Diky za pozvani a dalsi bodik."
+    urls = [
+        # "http://www.geocaching.com/geocache/GC286C1_vetrne-mlyny-na-decinsku-9-svetliny",
+        "http://www.geocaching.com/geocache/GC5NGBE_lesni-zabavna-stezka-13-finish",
+        "http://www.geocaching.com/geocache/GC5BERM_lesni-zabavna-stezka-11-cestou-do-podluzi",
+        "http://www.geocaching.com/geocache/GC5ACTP_lesni-zabavna-stezka-10-vylov-kesku",
+        "http://www.geocaching.com/geocache/GC5AQY9_lesni-zabavna-stezka-9-mechove-kameny",
+        "http://www.geocaching.com/geocache/GC5AAV6_lesni-zabavna-stezka-8-bozi-muka",
+        "http://www.geocaching.com/geocache/GC5AATC_lesni-zabavna-stezka-7-zajimava-kes",
+        "http://www.geocaching.com/geocache/GC5AARQ_lesni-zabavna-stezka-6-konciny-kostelniho-lesa",
+        "http://www.geocaching.com/geocache/GC5AQZA_lesni-zabavna-stezka-5-u-mytinky",
+        "http://www.geocaching.com/geocache/GC5AARB_lesni-zabavna-stezka-4-za-oznacenym-stromem",
+        "http://www.geocaching.com/geocache/GC5AANV_lesni-zabavna-stezka-3-u-vysilace",
+        "http://www.geocaching.com/geocache/GC5AAKT_lesni-zabavna-stezka-2-lesni-strazce",
+        "http://www.geocaching.com/geocache/GC5NGBB_lesni-zabavna-stezka-1-start",
+    ]
+    message = "Moc povedena serie! Odloveno s Karel55555, Radka55555 a vernou geofenou. Diky!"
     g = Geo()
     print 'login'
     g.login('', '')
 
     for link in urls:
         print 'logging: ' + link
-        g.log_cache(url=link, message=message, date_visited="20.Mar.2015")
+        g.log_cache(url=link, message=message, date_visited="24.May.2015")
