32c13a75fbdf7a2378156fba0e18dd0a06807f1a
x3mSpeedy <jan.hybs@tul.cz>
2015-09-13 17:18:49 +0200

    Add creds, fix old form names

---
+++ creds.py
@@ -0,0 +1,2 @@
+username = 'x3mSpeedy'
+password = ''
--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+import creds
+
 __author__ = 'x3mSpeedy'
 import urllib2
 import urllib
@@ -40,10 +45,9 @@ class Geo(object):
         self.br["ctl00$ContentBody$LogBookPanel1$uxLogInfo"] = message
 
         if tb:
-            self.br[
-                "ctl00$ContentBody$LogBookPanel1$uxTrackables$hdnSelectedActions"] = "4996238_Visited"
-            self.br["ctl00$ContentBody$LogBookPanel1$uxTrackables$repTravelBugs$ctl02$ddlAction"] = [
-                '4996238_Visited', ]
+            self.br["ctl00$ContentBody$LogBookPanel1$uxTrackables$hdnSelectedActions"] = "4996238_Visited,"
+            # self.br["ctl00$ContentBody$LogBookPanel1$uxTrackables$repTravelBugs$ctl02$ddlAction"] = [
+            #     '4996238_Visited', ]
 
         response = self.br.submit()
         html = response.read()
@@ -51,27 +55,45 @@ class Geo(object):
 
 
 if __name__ == "__main__":
-    urls = ['http://www.geocaching.com/geocache/GC4XHWT_dum-cesko-nemeckeho-porozumeni', 'http://www.geocaching.com/geocache/GC2KM4G_spoutana-nisa', 'http://www.geocaching.com/geocache/GC26DB4_areal-volnocasovych-aktivit-celakovskeho', 'http://www.geocaching.com/geocache/GC573ZY_izs-v-lk-stanice-hzs-jablonec-nad-nisou', 'http://www.geocaching.com/geocache/GC5HZJZ_pan-vajicko', 'http://www.geocaching.com/geocache/GC2N9PN_oko-bere', 'http://www.geocaching.com/geocache/GC1E0WP_hori', 'http://www.geocaching.com/geocache/GC1MY27_javor-ve-msene', 'http://www.geocaching.com/geocache/GC54KK9_sidliste-s-barvami-duhy', 'http://www.geocaching.com/geocache/GC2W50Q_pepicek-jde-do-skolky', 'http://www.geocaching.com/geocache/GC5BRV7_bobik', 'http://www.geocaching.com/geocache/GC4PYM3_kostel-nejsvetejsi-trojice', 'http://www.geocaching.com/geocache/GC4P2RZ_budka-pro-roryse', 'http://www.geocaching.com/geocache/GC2JCZF_draci-pribeh-i-evropska-legenda', 'http://www.geocaching.com/geocache/GC2M9AP_modra-je-dobra', 'http://www.geocaching.com/geocache/GC4ET7A_mseno-nad-nisou', 'http://www.geocaching.com/geocache/GC5GG9K_0-5-l-vody', 'http://www.geocaching.com/geocache/GC1DVEF_magicky-strom-magic-tree', 'http://www.geocaching.com/geocache/GC4PMVW_nefnukej-veverko', 'http://www.geocaching.com/geocache/GC2E3DR_paternity-2', 'http://www.geocaching.com/geocache/GC19GYE_prehradni-zahada', 'http://www.geocaching.com/geocache/GC2QK1P_tajvan', 'http://www.geocaching.com/geocache/GC2KN3R_singltrek', 'http://www.geocaching.com/geocache/GC1C8BP_haskovy-vily', 'http://www.geocaching.com/geocache/GC4FVQY_filmova', 'http://www.geocaching.com/geocache/GC1MF58_the-sculptures-of-jablonec-nad-nisou', 'http://www.geocaching.com/geocache/GC47J74_aprilek', 'http://www.geocaching.com/geocache/GC1KY0N_ceska-mincovna-the-czech-mint', 'http://www.geocaching.com/geocache/GCZYRN_prehrada-mseno-mseno-dam', 'http://www.geocaching.com/geocache/GC5JJZG_slunecni-hodiny', 'http://www.geocaching.com/geocache/GC2P455_rychla-smrt', 'http://www.geocaching.com/geocache/GC4EBY7_schody-do-pekla', 'http://www.geocaching.com/geocache/GC1P7NT_jizdarna-hippodrome', 'http://www.geocaching.com/geocache/GC2XG8M_lesni-tramvaj-ii-waldbim-ii']
     urls = [
-        # "http://www.geocaching.com/geocache/GC286C1_vetrne-mlyny-na-decinsku-9-svetliny",
-        "http://www.geocaching.com/geocache/GC5NGBE_lesni-zabavna-stezka-13-finish",
-        "http://www.geocaching.com/geocache/GC5BERM_lesni-zabavna-stezka-11-cestou-do-podluzi",
-        "http://www.geocaching.com/geocache/GC5ACTP_lesni-zabavna-stezka-10-vylov-kesku",
-        "http://www.geocaching.com/geocache/GC5AQY9_lesni-zabavna-stezka-9-mechove-kameny",
-        "http://www.geocaching.com/geocache/GC5AAV6_lesni-zabavna-stezka-8-bozi-muka",
-        "http://www.geocaching.com/geocache/GC5AATC_lesni-zabavna-stezka-7-zajimava-kes",
-        "http://www.geocaching.com/geocache/GC5AARQ_lesni-zabavna-stezka-6-konciny-kostelniho-lesa",
-        "http://www.geocaching.com/geocache/GC5AQZA_lesni-zabavna-stezka-5-u-mytinky",
-        "http://www.geocaching.com/geocache/GC5AARB_lesni-zabavna-stezka-4-za-oznacenym-stromem",
-        "http://www.geocaching.com/geocache/GC5AANV_lesni-zabavna-stezka-3-u-vysilace",
-        "http://www.geocaching.com/geocache/GC5AAKT_lesni-zabavna-stezka-2-lesni-strazce",
-        "http://www.geocaching.com/geocache/GC5NGBB_lesni-zabavna-stezka-1-start",
+        "http://www.geocaching.com/geocache/GC23E1A_milnik-milestone",
+        "http://www.geocaching.com/geocache/GC3TGXA_cisarska-alej",
+        "http://www.geocaching.com/geocache/GC2W5EW_rychlostnice-praha-liberec-night-edition",
+        "http://www.geocaching.com/geocache/GC1T9W2_skakal-pes-pres-oves",
+        "http://www.geocaching.com/geocache/GC573AC_walking-trail-1",
+        "http://www.geocaching.com/geocache/GC3QA3R_slunce-seno-a-par-facek",
+        "http://www.geocaching.com/geocache/GC573AD_walking-trail-2",
+        "http://www.geocaching.com/geocache/GC573AJ_walking-trail-3",
+        "http://www.geocaching.com/geocache/GC573AR_walking-trail-4-dobrodruzstvi-pavouka-stestika",
+        "http://www.geocaching.com/geocache/GC573AW_walking-trail-5-liska-bystrouska",
+        "http://www.geocaching.com/geocache/GC573B0_walking-trail-6-slepice-domaci",
+        "http://www.geocaching.com/geocache/GC573B9_walking-trail-7-skokan-hndy",
+        "http://www.geocaching.com/geocache/GC573BE_walking-trail-8-u-krtka",
+        "http://www.geocaching.com/geocache/GC573BP_walking-trail-9-parezova-chaloupka",
+        "http://www.geocaching.com/geocache/GC573BZ_walking-trail-10-pod-jezevci-skalou",
+        "http://www.geocaching.com/geocache/GC573C4_walking-trail-11-vres-obecny",
+        "http://www.geocaching.com/geocache/GC573CA_walking-trail-12-kachna-obecna",
+        "http://www.geocaching.com/geocache/GC3VYK4_zapomenute-jezirko-2",
+        "http://www.geocaching.com/geocache/GC573CF_walking-trail-13",
+        "http://www.geocaching.com/geocache/GC3VP9J_zapomenute-jezirko",
+        "http://www.geocaching.com/geocache/GC573CK_walking-trail-14-sycek-obecny",
+        "http://www.geocaching.com/geocache/GC573CX_walking-trail-15-jezek-vychodni",
+        "http://www.geocaching.com/geocache/GC573D1_walking-trail-16",
+        "http://www.geocaching.com/geocache/GC573D5_walking-trail-17",
+        "http://www.geocaching.com/geocache/GC573D8_walking-trail-18",
+        "http://www.geocaching.com/geocache/GC2HZN7_tajemstvi-archivu-dr-watsona",
+        "http://www.geocaching.com/geocache/GC573DG_walking-trail-19",
+        "http://www.geocaching.com/geocache/GC573DV_walking-trail-20-mravenec-lesni",
+        "http://www.geocaching.com/geocache/GC58CYD_strycek-skrblik-bonus",
+        "http://www.geocaching.com/geocache/GC587KT_kacer-uu",
+        # "http://www.geocaching.com/geocache/GC58AD1_kaceri-pribehy"
     ]
-    message = "Moc povedena serie! Odloveno s Karel55555, Radka55555 a vernou geofenou. Diky!"
+    urls.reverse()
+    message = "Bezvadný výlet s partou pohodových geokačerů. Přálo nám počasí a udělali jsme 32 kousků. Při cestě nechyběla legrace ani nadšení. Díky moc za kešku!"
     g = Geo()
     print 'login'
-    g.login('', '')
+    g.login(creds.username, creds.password)
 
     for link in urls:
         print 'logging: ' + link
-        g.log_cache(url=link, message=message, date_visited="24.May.2015")
+        g.log_cache(url=link, message=message, date_visited="03.May.2015")
